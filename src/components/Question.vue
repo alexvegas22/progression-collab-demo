84312b76841b96fb3ea193b86341a186ff30700a
Alexandre Dinh <1454224@crosemont.qc.ca>
2021-02-11 16:03:32 -0500

    Ajout bouton obtention d'un énoncé

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="question">
-      <h1>Question</h1>
+    <h1>Question</h1>
+    <button v-on:click="obtenirUneQuestion">Obtenir une question</button>
+    <h2>{{ enonce }}</h2>
   </div>
 </template>
