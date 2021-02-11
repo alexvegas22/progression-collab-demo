f2ece60ce0a830248c042b91b5ac9312bc82801f
Danny Villeda <1341188@crosemont.qc.ca>
2021-02-11 18:27:18 -0500

    Correction a Question.vue pour utiliser axios correctement. Delete des scripts JS inutile.

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,39 +1,39 @@
 <template>
   <div id="Retroaction">
-      {{sortieObserver}}
-      <h1>TEST</h1>
-    <button v-on:click="retro()">Cliquer</button>
+    <h1>Rétroaction</h1>
+    <button v-on:click="obtenirRetroaction">Cliquer</button></br>
+    <h2>Votre solution est: </br>{{solution}}</h2>
+    <h2>Voici les tests effectués: </br>{{test}}</h2>
   </div>
 </template>
 
 <script>
-import Thèmes from './Thème.vue'
-import fetchapi from '../util/question'
-import RetroJS from '../util/questionjs';
+import axios from "axios";
 
 export default {
-    name: 'Question',
-    components: {
-        RetroJS
-    },
-    props: {
+  name: "Question",
 
-    },
-    data() {
-      return {
+  data() {
+    return {
+      solution: "",
+      test:"",
+    };
+  },
 
-      }
+  methods: {
+    obtenirRetroaction() {
+      this.sortieObserver = axios
+        .get(
+          "https://fb21cf46-76f2-4f2c-ae0d-2d2aa69baf67.mock.pstmn.io/retroaction"
+        )
+        .then((response) => {
+          this.solution = response.data.Solution;
+          this.test = response.data.Test;
+        })
+        .catch((e) => {
+          this.errors.push(e);
+        });
     },
-    methods: {
-        retro() {
-        fetchapi("username","password").then(
-            Test => {
-                this.sortieAttendue = sortie_attendue;
-                this.sortieObserver = sortie_observée;
-                this.sortieErreur = sortie_erreur;
-                }
-            )
-        }
-    },
+  },
 };
 </script>
