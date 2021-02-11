24a120295ec63f4600489b45d78c69619e974e7c
Samuel Beaulieu <6149537@crosemont.qc.ca>
2021-02-11 17:01:21 -0500

    ajout script avec axios pour récupérer un énoncé de question

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -5,3 +5,32 @@
     <h2>{{ enonce }}</h2>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Question",
+
+  data() {
+    return {
+      enonce: "",
+    };
+  },
+
+  methods: {
+    obtenirUneQuestion() {
+      this.enonce = axios
+        .get(
+          "https://fb21cf46-76f2-4f2c-ae0d-2d2aa69baf67.mock.pstmn.io/question"
+        )
+        .then((response) => {
+          this.enonce = response.data.Question.énoncé;
+        })
+        .catch((e) => {
+          this.errors.push(e);
+        });
+    },
+  },
+};
+</script>
