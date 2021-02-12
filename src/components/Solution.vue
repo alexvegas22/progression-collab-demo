8eee35df3c548aa1eb3306f7e5cb0e8120536830
Sebastien Vermandele <sebvermandele@gmail.com>
2021-02-11 22:24:43 -0500

    create a new component to display one solution by date_soumission

---
+++ src/components/Solution.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h3>Une solution</h3>
+    <p>Langage id: {{langage}}</p>
+  </div>
+  
+  
+
+</template>
+
+
+<script>
+import get_solution from '../util/solution'
+
+export default {
+  data(){
+    return {
+      date_soumission: "",
+      langage: "",
+      code: "",
+      feedback: ""
+    };
+  },
+  created(){
+    get_solution(1613094107)
+          .then(
+            response=>{
+              this.date_soumission = response.date_soumission;
+              this.langage = response.langage;
+              this.code = response.code;
+              this.feedback = response.feedback
+            })          
+          .catch(err=>{
+              this.erreurs = err;
+          }); 
+    
+  }
+}  
+</script>
