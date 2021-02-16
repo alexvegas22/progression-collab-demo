34906988900d5cb7518bac5fbc8d3b80b70a8822
Samuel Beaulieu <6149537@crosemont.qc.ca>
2021-02-16 17:02:38 -0500

    Migration vers VueJs 3.0.5

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,30 @@
 <template>
-  <div id="app">
-    <Login password_req="true" domaine_mail="crosemont.qc.ca" />
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
   </div>
+  <router-view/>
 </template>
 
-<script>
-import Login from "./components/Login.vue";
-import Énoncé from "./components/Énoncé.vue";
-
-export default {
-  name: "App",
-  components: {
-    Login,
-    Énoncé,
-  },
-};
-</script>
-
-<style src="./css/style.css">
-<style > #app {
+<style>
+#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
