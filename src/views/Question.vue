3656d11312bdd00208452a0db295a2da330abd62
Massy Haddad <massy.haddad@lepisc.com>
2021-02-17 18:03:03 -0500

    Migration vers Vue 3

---
+++ src/views/Question.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="question">
+        <h3><strong>Question : {{question}}</strong></h3>
+        <!--
+        <div class="ebauche" v-bind:key="ebauche" v-for="ebauche in ebauches">
+            <p>{{ebauche}}</p>
+        </div>
+        -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import get_question from '../util/question'
+
+export default {
+  name: 'Question',
+  
+  components: {
+     data() {
+        return {
+            question: get_question('programmation_1', 'les_variables', 'introduction_aux_variables', 'python').then(
+                response => {
+                    this.question = response;
+                }
+            ).catch(
+                err => {
+                    console.log(err);
+                }
+            )
+        }
+     }
+  }
+}
+</script>
+
