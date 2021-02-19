a0d67b2cb59f3355aa6b48ec58a94abb4ce4b265
Julien Jacquard <jacquardjul@gmail.com>
2021-02-18 21:46:48 -0500

    suppression du selecteur #App non utilisé dans la page SolutionsView.vue

--- src/views/SolutionsView.vue
+++ src/views/SolutionsView.vue
@@ -75,13 +75,4 @@ li{
   float: left;
 }
 
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
