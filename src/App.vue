53b2166c5de830aad3d82456633d273032504d61
Sebastien Vermandele <sebvermandele@gmail.com>
2021-02-18 19:39:40 -0500

    add new link to the home page

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,8 @@
   <div id="nav">
 
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/solutions">Solutions</router-link>
 
   </div>
   <router-view/>
