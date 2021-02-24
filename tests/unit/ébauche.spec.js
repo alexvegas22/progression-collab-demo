c701b10636b4b1e6e487757a40320e2d133e160e
angel <angel.ir.arevalo@gmail.com>
2021-02-24 12:38:18 -0500

    tests de test

--- tests/unit/ébauche.spec.js
+++ tests/unit/ébauche.spec.js
@@ -1,20 +1,24 @@
 import {render, screen, fireEvent} from '@testing-library/vue'
-import ebauche from '@/components/Question/Ebauche'
-import get_question from "@/util/question";
+import {get_question} from "@/util/question";
 
-test('Étant donnée une question, lorsque la page est chargée, alors l\'ébauche est visible', async () => {
+/*test('Étant donnée une question, lorsque la page est chargée, alors l\'ébauche est visible', async () => {
 
-  ebauches:[] // liste d'ébauche
+  render(ebauche)
 
-  question: get_question('programmation_1', 'les_variables', 'introduction_aux_variables', 'python').then(
-          response => {
-            this.question = response;
-            this.ebauches = response.question_prog.ébauches;
-          }
-      )
-  // eslint-disable-next-line no-undef
-  expect(ebauches[0]).toBe(" print(réponse)")
+  expect(ebauche.data().ebauches).toBe("print(réponse)")
 })
+
+ */
+test('api method charger une tentative', () => {
+  let questionAttendue = {"ébauches": [
+      "\nprint(réponse)",
+      "\nprint(AutreRéponse)"]};
+
+  get_question.then((data) => {
+    expect(data).toBe(questionAttendue.ébauches[0])
+  })
+})
+
 
 
 test('test pour tester que les tests fonctionnent', async () => {
