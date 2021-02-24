261d9912246c967580989891a6dd93f7058eb0f8
angel <angel.ir.arevalo@gmail.com>
2021-02-24 11:23:04 -0500

    fix de l'import d'ebauche

--- tests/unit/ébauche.spec.js
+++ tests/unit/ébauche.spec.js
@@ -1,18 +1,23 @@
 import {render, screen, fireEvent} from '@testing-library/vue'
-import ebauche from 'src/components/Question/Ebauche'
+import ebauche from '@/components/Question/Ebauche'
+import get_question from "@/util/question";
 
 test('Étant donnée une question, lorsque la page est chargée, alors l\'ébauche est visible', async () => {
-  // The `render` method renders the component into the document.
-  // It also binds to `screen` all the available queries to interact with
-  // the component.
-  render(ebauche)
 
-  // queryByText returns the first matching node for the provided text
-  // or returns null.
-  expect(screen.queryByText('print(réponse)')).toBeTruthy()
+  ebauches:[] // liste d'ébauche
+
+  question: get_question('programmation_1', 'les_variables', 'introduction_aux_variables', 'python').then(
+          response => {
+            this.question = response;
+            this.ebauches = response.question_prog.ébauches;
+          }
+      )
+  // eslint-disable-next-line no-undef
+  expect(ebauches[0]).toBe(" print(réponse)")
 })
+
 
 test('test pour tester que les tests fonctionnent', async () => {
   let laVar = "un texte qui est un string";
-  expect(laVar).toBeTruthy()
+  expect(laVar).toBe("un texte qui est un string")
 })
