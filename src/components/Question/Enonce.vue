41dbac7d2889b99507685be0283d091443d6962d
Samuel Beaulieu <6149537@crosemont.qc.ca>
2021-03-09 17:52:24 -0500

    Ajout de styles CSS pour le composant Énoncé

--- src/components/Question/Enonce.vue
+++ src/components/Question/Enonce.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="enonce">
-    <h1>{{ titre }}</h1>
-    <h2>{{ enonce }}</h2>
+    <h3 class="titre">{{ titre }}</h3>
+    <br />
+    <p class="enonce">{{ enonce }}</p>
   </div>
 </template>
 
@@ -11,3 +12,26 @@ export default {
   props: ["titre", "enonce"]
 };
 </script>
+
+<style>
+  #enonce {
+    margin: 10px;
+    padding: 20px;
+    border: 2px solid black;
+    border-radius: 20px; 
+    background-color: #2d2d2d;
+  }
+
+  .enonce {
+    font-style: italic;
+    font-size: 1.2em;
+    color: lightgrey;
+  }
+
+  .titre {
+    text-align: center;
+    color: grey
+  }
+  
+
+</style>>
