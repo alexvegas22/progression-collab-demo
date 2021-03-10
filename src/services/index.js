9feb4f54bcce21efe1ac4625b980ab9df7e216e3
Samuel Beaulieu <6149537@crosemont.qc.ca>
2021-03-10 16:48:47 -0500

    ajout de l'architecture VueX

---
+++ src/services/index.js
@@ -0,0 +1,11 @@
+import request_service, { getData } from './request_services'
+const BASE_URL = process.env.VUE_APP_API_URL_QUESTION // json-server
+
+// TODO : Vérifier si on laisse le catch ici (s'il est utile) puisqu'on le placera dans Actions.js aussi
+const getQuestion = function () {
+    return getData(BASE_URL).then(
+        data => { return data }
+    ).catch((err) => {
+        reject(err);
+    });
+}
