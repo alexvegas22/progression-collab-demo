8272f76b57099700df8115bbd216f2bdc547cd0e
Samuel Beaulieu <6149537@crosemont.qc.ca>
2021-03-11 16:14:11 -0500

    suppression de lignes de code superflues

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,20 +1,6 @@
 export const mutations = {
     setQuestion (state, question) {
       state.question = question
-    },
-  
-    removeTodo (state, todo) {
-      state.todos.splice(state.todos.indexOf(todo), 1)
-    },
-  
-    editTodo (state, { todo, text = todo.text, done = todo.done }) {
-      const index = state.todos.indexOf(todo)
-  
-      state.todos.splice(index, 1, {
-        ...todo,
-        text,
-        done
-      })
     }
   }
   
