8b80796f08b5aa17fb19d1626a7b15f1d97b481c
Danny Villeda <dannyavilleda@gmail.com>
2021-03-11 09:41:16 -0500

    Creation de composantes de base pour le jeu de tests.

---
+++ src/components/Question/Test.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <AffichageValidation />
+    </div>
+</template>
+<script>
+import AffichageValidation from './AffichageValidation.vue'
+export default {
+  components: { AffichageValidation },
+    name:"Test",
+}
+</script>
