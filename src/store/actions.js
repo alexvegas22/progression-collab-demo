95a189fd0c3fc51a7a8d6b32fbc4186c5f921d4b
Simon Roy <simon.roy1211@gmail.com>
2021-03-15 14:16:41 -0400

    Url ebauche passé en paramètre à getEbauche

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,19 +2,17 @@ import {getQuestion} from '../services'
 import { getEbaucheApi } from "../services/index";
 
 export default {
-  async getQuestion({commit, dispatch}, url) {
+  async getQuestion({ commit, dispatch, state }, url) {
     try{
       commit('setQuestion', await getQuestion(url))
 
-      await dispatch('getEbauche');
+      await dispatch('getEbauche', state.question.relationships.ebauches.links.related);
     } catch (error){
       console.log(error)
     }
   },
-  async getEbauche({ commit, state }) {
+  async getEbauche({ commit }, ebaucheUrl) {
     try {
-      const ebaucheUrl = state.question.relationships.ebauches.links.related
-
       commit("setEbauche", await getEbaucheApi(ebaucheUrl));
     } catch (error) {
       console.log(error);
