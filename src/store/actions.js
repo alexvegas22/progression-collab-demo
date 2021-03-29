e39d087245c7ae140f2fbbb1a283bebd34db511d
Hamed Tijane Mkpoumie Monfouayi <hmkpoumiemonfouayi@dept-info.crosemont.quebec>
2021-03-29 10:37:58 -0400

    Correction mineure dans Action.js et ajout d'un TODO

--- src/store/actions.js
+++ src/store/actions.js
@@ -25,11 +25,12 @@ export default {
     commit("updateMsgAPIEnvoiTentative", "Envoie de la tentative en cours..");
     try {
       var retroactionTentative = await postTentative({ langage }, { code });
+      //TODO: Le résultat d'un post à /tentative est la Tentative soumise et traitée. Ça devrait donc probablement être setTentative, ici.
       commit("updateRetroaction", retroactionTentative);
       commit("updateMsgAPIEnvoiTentative", null);
       commit("updateEnvoieTentativeEnCours", false);
     } catch (error) {
-      commit("updateMsgAPIEnvoiTentative", "Impossible de comminuquer avec le super server");
+      commit("updateMsgAPIEnvoiTentative", "Impossible de communiquer avec le serveur");
       commit("updateEnvoieTentativeEnCours", false);
       console.log(error);
     }
