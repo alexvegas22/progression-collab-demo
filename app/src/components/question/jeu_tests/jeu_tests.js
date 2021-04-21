d8989cf0c4bffc63ceb58fab1d9285a35b4cdbf1
Radu Octavian Dumitru <raduoctav31@protonmail.com>
2021-04-21 16:52:04 -0400

    Ajout remplacement des caractères pour mode visuel

--- app/src/components/question/jeu_tests/jeu_tests.js
+++ app/src/components/question/jeu_tests/jeu_tests.js
@@ -10,5 +10,31 @@ export default {
 		resultats() {
 			return this.$store.state.retroactionTentative ? this.$store.state.retroactionTentative.resultats : [];
 		},
+		data: {
+			modeVisuel: false,
+		},
+	},
+	methods: {
+		remplacerCaractèresVisuels(chaîne) {
+			if (!chaîne) {
+				return;
+			}
+
+			if (this.modeVisuel) {
+				chaîne = chaîne.replace(/ /g, '<span class="modeVisuel">_</span>');
+				chaîne = chaîne.replace(/\r|\n/g, '<span class="modeVisuel">↵\n</span>');
+			} else {
+				chaîne = chaîne.replace(/<span class="modeVisuel">_<\/span>/g, " ");
+				chaîne = chaîne.replace(/<span class="modeVisuel">↵\n<\/span>/g, "\n");
+			}
+			return chaîne;
+		},
+		changerModeComparaison() {
+			this.modeVisuel = !this.modeVisuel;
+			for (let i = 0; i < this.resultats.length; i++) {
+				this.resultats[i].sortie_observée = this.remplacerCaractèresVisuels(this.resultats[i].sortie_observée);
+				this.tests[i].sortie_attendue = this.remplacerCaractèresVisuels(this.tests[i].sortie_attendue);
+			}
+		},
 	},
 };
--- app/src/components/question/jeu_tests/jeu_tests.vue
+++ app/src/components/question/jeu_tests/jeu_tests.vue
@@ -1,8 +1,20 @@
 <template>
-	<h1 style="text-align: center">{{$t('jeu_tests.jeuTests')}}</h1>
+	<h1 style="text-align: center">{{ $t("jeu_tests.jeuTests") }}</h1>
+	<div class="text-center">
+		<label class="label">
+			<div class="label-text" style="margin-right: 10px">Normal</div>
+			<div class="toggle">
+				<input class="toggle-state" type="checkbox" name="check" value="check" @change="changerModeComparaison()" />
+				<div class="indicator"></div>
+			</div>
+			<div class="label-text">Visuel</div>
+		</label>
+	</div>
 	<div v-for="(test, index) in tests" :key="index">
 		<Test v-bind:test="test" v-bind:resultat="resultats[index]" />
 	</div>
 </template>
 
 <script src="./jeu_tests.js"></script>
+
+<style src="./jeu_tests.css"></style>
