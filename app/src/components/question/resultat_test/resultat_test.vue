15de3a746b17f656ede11c0ac588eb34d2cdcde5
Radu Octavian Dumitru <raduoctav31@protonmail.com>
2021-04-21 12:21:07 -0400

    Ajout élément distinctif pour la sortie vide

--- app/src/components/question/resultat_test/resultat_test.vue
+++ app/src/components/question/resultat_test/resultat_test.vue
@@ -3,14 +3,16 @@
 		<div class="row">
 			<div class="bg-light col card device-card">
 				<div class="card-body">
-					<h5 class="text-black-50 font-weight-bold text-center card-title">{{$t('resultat_test.entrée')}}</h5>
+					<h5 class="text-black-50 font-weight-bold text-center card-title">{{ $t("resultat_test.entrée") }}</h5>
 					<p class="card-text" v-html="test.entrée"></p>
 				</div>
 			</div>
 
 			<div class="bg-light col card device-card">
 				<div class="card-body">
-					<h5 class="text-black-50 font-weight-bold text-center card-title">{{$t('resultat_test.sortieAttendue')}}</h5>
+					<h5 class="text-black-50 font-weight-bold text-center card-title">
+						{{ $t("resultat_test.sortieAttendue") }}
+					</h5>
 					<pre>
 						<p class="card-text" v-html="test.sortie_attendue"></p>
 					</pre>
@@ -22,7 +24,7 @@
 			<div class="row">
 				<div v-if="resultat.sortie_erreur" class="bg-light col card device-card">
 					<div class="card-body">
-						<h5 class="text-danger font-weight-bold text-center card-title">{{$t('resultat_test.sortieErreur')}}</h5>
+						<h5 class="text-danger font-weight-bold text-center card-title">{{ $t("resultat_test.sortieErreur") }}</h5>
 						<pre>
 							<p class="card-text" v-html="resultat.sortie_erreur"></p>
 						</pre>
@@ -31,8 +33,13 @@
 
 				<div class="bg-light col card device-card">
 					<div class="card-body">
-						<h5 class="text-black-50 font-weight-bold text-center card-title">{{$t('resultat_test.sortieConsole')}}</h5>
-						<pre>
+						<h5 class="text-black-50 font-weight-bold text-center card-title">
+							{{ $t("resultat_test.sortieConsole") }}
+						</h5>
+						<pre v-if="resultat.sortie_observée == ''">
+							<p class="card-text resultat vide">[vide]</p>
+						</pre>
+						<pre v-else>
 							<p class="card-text" v-html="resultat.sortie_observée"></p>
 						</pre>
 					</div>
@@ -42,7 +49,7 @@
 			<div class="row">
 				<div v-if="resultat.feedback" class="bg-light col card device-card">
 					<div class="card-body">
-						<h5 class="text-black-50 font-weight-bold text-center card-title">{{$t('resultat_test.rétroaction')}}</h5>
+						<h5 class="text-black-50 font-weight-bold text-center card-title">{{ $t("resultat_test.rétroaction") }}</h5>
 						<p class="card-text" v-html="resultat.feedback"></p>
 					</div>
 				</div>
@@ -52,3 +59,5 @@
 </template>
 
 <script src="./resultat_test.js"></script>
+
+<style src="./resultat_test.css"></style>
