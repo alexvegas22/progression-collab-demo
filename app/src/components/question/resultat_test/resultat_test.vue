1843dfbe9e351cd44ad67701483d5022ca84d212
Radu Octavian Dumitru <raduoctav31@protonmail.com>
2021-04-21 12:57:52 -0400

    Re-mise en page des sorties

--- app/src/components/question/resultat_test/resultat_test.vue
+++ app/src/components/question/resultat_test/resultat_test.vue
@@ -3,17 +3,34 @@
 		<div class="row">
 			<div class="bg-light col card device-card">
 				<div class="card-body">
-					<h5 class="text-black-50 font-weight-bold text-center card-title">{{$t('resultat_test.entrée')}}</h5>
+					<h5 class="text-black-50 font-weight-bold text-center card-title">{{ $t("resultat_test.entrée") }}</h5>
 					<p class="card-text" v-html="test.entrée"></p>
 				</div>
 			</div>
+		</div>
 
+		<div class="row">
 			<div class="bg-light col card device-card">
 				<div class="card-body">
-					<h5 class="text-black-50 font-weight-bold text-center card-title">{{$t('resultat_test.sortieAttendue')}}</h5>
+					<h5 class="text-black-50 font-weight-bold text-center card-title">
+						{{ $t("resultat_test.sortieAttendue") }}
+					</h5>
 					<pre>
 						<p class="card-text" v-html="test.sortie_attendue"></p>
 					</pre>
+				</div>
+			</div>
+
+			<div v-if="resultat">
+				<div class="bg-light col card device-card">
+					<div class="card-body">
+						<h5 class="text-black-50 font-weight-bold text-center card-title">
+							{{ $t("resultat_test.sortieConsole") }}
+						</h5>
+						<pre>
+							<p class="card-text" v-html="resultat.sortie_observée"></p>
+						</pre>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -22,18 +39,9 @@
 			<div class="row">
 				<div v-if="resultat.sortie_erreur" class="bg-light col card device-card">
 					<div class="card-body">
-						<h5 class="text-danger font-weight-bold text-center card-title">{{$t('resultat_test.sortieErreur')}}</h5>
+						<h5 class="text-danger font-weight-bold text-center card-title">{{ $t("resultat_test.sortieErreur") }}</h5>
 						<pre>
 							<p class="card-text" v-html="resultat.sortie_erreur"></p>
-						</pre>
-					</div>
-				</div>
-
-				<div class="bg-light col card device-card">
-					<div class="card-body">
-						<h5 class="text-black-50 font-weight-bold text-center card-title">{{$t('resultat_test.sortieConsole')}}</h5>
-						<pre>
-							<p class="card-text" v-html="resultat.sortie_observée"></p>
 						</pre>
 					</div>
 				</div>
@@ -42,7 +50,7 @@
 			<div class="row">
 				<div v-if="resultat.feedback" class="bg-light col card device-card">
 					<div class="card-body">
-						<h5 class="text-black-50 font-weight-bold text-center card-title">{{$t('resultat_test.rétroaction')}}</h5>
+						<h5 class="text-black-50 font-weight-bold text-center card-title">{{ $t("resultat_test.rétroaction") }}</h5>
 						<p class="card-text" v-html="resultat.feedback"></p>
 					</div>
 				</div>
