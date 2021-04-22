28f398216174bc5ee0b21b9d049e44042acbd69f
Radu Octavian Dumitru <raduoctav31@protonmail.com>
2021-04-21 20:17:35 -0400

    Ajout traduction des modes

--- app/src/components/question/jeu_tests/jeu_tests.vue
+++ app/src/components/question/jeu_tests/jeu_tests.vue
@@ -2,12 +2,12 @@
 	<h1 style="text-align: center">{{ $t("jeu_tests.jeuTests") }}</h1>
 	<div v-if="resultats[0]" class="text-center">
 		<label class="label">
-			<div class="label-text" style="margin-right: 10px">Normal</div>
+			<div class="label-text" style="margin-right: 10px">{{ $t("jeu_tests.modeNormal") }}</div>
 			<div class="toggle">
 				<input class="toggle-state" type="checkbox" name="check" value="check" @change="changerModeComparaison()" />
 				<div class="indicator"></div>
 			</div>
-			<div class="label-text">Visuel</div>
+			<div class="label-text">{{ $t("jeu_tests.modeVisuel") }}</div>
 		</label>
 	</div>
 	<div v-for="(test, index) in tests" :key="index">
