beb84671ee8ee46e83f191d6c3df9b66ecd8081d
hbarou <1536306@crosemont.qc.ca>
2021-04-28 11:55:08 -0400

    Reformatage des noms de variables et de méthodes

--- app/src/util/i18n.js
+++ app/src/util/i18n.js
@@ -1,34 +1,33 @@
 import { createI18n } from "vue-i18n/index";
 
-function loadLocaleMessages() {
+function chargerMessagesLocale() {
 	const locales = require.context("../locales", true, /[A-Za-z0-9-_,\s]+\.json$/i);
 	const messages = {};
 	locales.keys().forEach(key => {
-		const matched = key.match(/([A-Za-z0-9-_]+)\./i);
-		if (matched && matched.length > 1) {
-			const locale = matched[1];
+		const langueTrouvé = key.match(/([A-Za-z0-9-_]+)\./i);
+		if (langueTrouvé && langueTrouvé.length > 1) {
+			const locale = langueTrouvé[1];
 			messages[locale] = locales(key);
 		}
 	});
 	return messages;
 }
 
-function checkDefaultLanguage() {
-	let matched = null;
-	let languages = Object.getOwnPropertyNames(loadLocaleMessages());
-	languages.forEach(lang => {
-		let languagePartials = navigator.language.split('-')[0];
-		if (lang === languagePartials) {
-			matched = lang;
+function trouverLocale(langueNavigateur) {
+	let langueTrouvé = null;
+	let langages = Object.getOwnPropertyNames(chargerMessagesLocale());
+	langages.forEach(langue => {
+		if (langue === langueNavigateur) {
+			langueTrouvé = langue;
 		}
 	});
-	return matched;
+	return langueTrouvé;
 }
 
 const i18n = new createI18n({
-	locale: checkDefaultLanguage() || process.env.VUE_APP_I18N_LOCALE,
+	locale: trouverLocale(navigator.language.split('-')[0]) || process.env.VUE_APP_I18N_LOCALE,
 	fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE,
-	messages: loadLocaleMessages()
+	messages: chargerMessagesLocale()
 });
 
 export default i18n;
