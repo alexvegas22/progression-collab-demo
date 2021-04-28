35adf73c54e83b7269975d339f8fef51b4f605b5
Patrick Lafrance <patrick.lafrance@gmail.com>
2021-04-28 18:58:51 -0400

    Petite refactorisation

--- app/src/util/i18n.js
+++ app/src/util/i18n.js
@@ -1,33 +1,29 @@
 import { createI18n } from "vue-i18n/index";
 
-function chargerMessagesLocale() {
+const messages = chargerMessagesLocalisés();
+
+function chargerMessagesLocalisés() {
 	const locales = require.context("../locales", true, /[A-Za-z0-9-_,\s]+\.json$/i);
 	const messages = {};
 	locales.keys().forEach(key => {
-		const langueTrouvé = key.match(/([A-Za-z0-9-_]+)\./i);
-		if (langueTrouvé && langueTrouvé.length > 1) {
-			const locale = langueTrouvé[1];
+		const langueTrouvée = key.match(/([A-Za-z0-9-_]+)\./i);
+		if (langueTrouvée && langueTrouvée.length > 1) {
+			const locale = langueTrouvée[1];
 			messages[locale] = locales(key);
 		}
 	});
 	return messages;
 }
 
-function trouverLocale(langueNavigateur) {
-	let langueTrouvé = null;
-	let langages = Object.getOwnPropertyNames(chargerMessagesLocale());
-	langages.forEach(langue => {
-		if (langue === langueNavigateur) {
-			langueTrouvé = langue;
-		}
-	});
-	return langueTrouvé;
+function trouverLocalePréférée(languePréférée) {
+	let langages = Object.getOwnPropertyNames(messages);
+	return langages.includes(languePréférée) ? languePréférée : process.env.VUE_APP_I18N_LOCALE;
 }
 
 const i18n = new createI18n({
-	locale: trouverLocale(navigator.language.split('-')[0]) || process.env.VUE_APP_I18N_LOCALE,
+	locale: trouverLocalePréférée(navigator.language.split('-')[0]),
 	fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE,
-	messages: chargerMessagesLocale()
+	messages: messages
 });
 
 export default i18n;
