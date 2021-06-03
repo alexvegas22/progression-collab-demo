38cca8246b6fb90d2c26c5cac68db7ed4a97e043
Patrick Lafrance <patrick.lafrance@gmail.com>
2021-06-03 19:02:44 -0400

    Ajout du highlighting dans les énoncés MD

--- app/src/util/parse.js
+++ app/src/util/parse.js
@@ -1,13 +1,29 @@
-const MarkDown = require("markdown-it")({
+var hljs = require('highlight.js'); // https://highlightjs.org/
+
+// Actual default values
+var md = require('markdown-it')({
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return '<pre class="hljs"><code>' +
+               hljs.highlight(str, { language: lang, ignoreIllegals: true }).value +
+               '</code></pre>';
+      } catch (e) {
+          console.log(e);
+      }
+    }
+
+    return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
+  },
 	// Évite les attaques XSS qui pourraient être introduites dans des questions malveillantes.
 	html: false,
-}).use(require("markdown-it-imsize"));
+}).use(require('markdown-it-imsize'));
 
 const parseMD = (data) => {
 	if (!data) {
 		return "";
 	} else {
-		return MarkDown.render(data);
+		return md.render(data);
 	}
 };
 
