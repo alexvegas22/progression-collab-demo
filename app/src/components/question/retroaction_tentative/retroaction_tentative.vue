c455e9416e1acc4bcee98dc613d145b7f8a6fbeb
Patrick Lafrance <patrick.lafrance@gmail.com>
2021-06-04 09:51:09 -0400

    Amélioration du popup conseil et de la barre de progression

--- app/src/components/question/retroaction_tentative/retroaction_tentative.vue
+++ app/src/components/question/retroaction_tentative/retroaction_tentative.vue
@@ -1,16 +1,15 @@
 <template>
-	<div v-if="tentative || tentativeEnCoursDeSoumission" >
-		<div v-if="retroactionTentative" class="progress">
+	<div>
+		<div class="progress">
 			<div v-bind:style="testsRéussisPct" class="progress-bar test_réussi" role="progressbar" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"/>
 			<div v-bind:style="testsRatésPct" class="progress-bar test_non_réussi" role="progressbar" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"/>
 		</div>
 
-		<tippy :interactive="true" class="popup_conseil">
+		<tippy v-if="retroactionTentative" :interactive="true" :showOnCreate="true" :arrow="true" class="popup_conseil">
 			<a id="btn_conseil" v-show="retroactionTentative && retroactionTentative.feedback">💡</a>
 
 			<template #content>
-				<div class="fond_conseil">
-					<div v-show="msgReponseApi">
+					<div v-if="msgReponseApi">
 						<strong>{{ $t("retroaction_tentative."+msgReponseApi) }}</strong>
 					</div>
 					<div v-if="retroactionTentative">
@@ -24,7 +23,6 @@
 					<div v-else>
 						{{$t('retroaction_tentative.soumettre')}}
 					</div>
-				</div>
 			</template>
 		</tippy>
 	</div>
