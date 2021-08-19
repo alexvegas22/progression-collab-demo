e656942840ec3c9e2612688d2a8c96e82fe29d48
Patrick Lafrance <plafrance@crosemont.qc.ca>
2021-08-18 21:10:36 -0400

    ajout de la vue de login

---
+++ app/src/components/login/login.js
@@ -0,0 +1,36 @@
+import login_get_token from "@/util/login";
+
+const API_URL = process.env.VUE_APP_API_URL;
+
+export default {
+	name: "Login",
+	props: {
+		password_req: String,
+		domaine_mail: String,
+	},
+	data() {
+		return {
+			username: "",
+			password: "",
+			logué: "",
+			erreurs: "",
+		};
+	},
+	methods: {
+		login() {
+			const { username, password } = this;
+			login_get_token(username, password)
+				.then((token) => {
+					this.logué = "true";
+					this.erreurs = "";
+					this.$store.dispatch("setToken", token.data.Token);
+					this.$store.dispatch("getUser", API_URL + "/user/" + username)
+				})
+				.catch((err) => {
+					this.erreurs = err;
+					this.logué = "";
+					this.$store.dispatch("deleteToken");
+				});
+		},
+	},
+};
---
+++ app/src/views/login/login.vue
@@ -0,0 +1,42 @@
+<template>
+	<div>
+		<Login @logué="onLogué" password_req="true" />
+		<LTI context="context"/>
+	</div>
+</template>
+
+<script>
+ import Login from "@/components/login/login.vue";
+ //import LTI from "@/components/login/liaisonLTI.vue";
+
+ export default {
+	 name: "LoginView",
+	 components: {
+		 Login,
+		 //LTI
+	 },
+	 props: {
+		 ref: String,
+		 ltik: String,
+		 context: String,
+	 },
+	 methods: {
+		 onLogué(token, cléApi){
+			 if (token) {
+				 if (cléApi){
+					 this.envoyerCléApi(token, cléApi)
+				 }
+				 this.envoyerToken(token)
+			 }
+
+			 this.rediriger();
+		 },
+		 rediriger(){
+			 this.$router.push(this.ref);
+		 },
+		 envoyerToken(){},
+		 envoyerCléApi(){},
+	 }
+ 
+ };
+</script>
