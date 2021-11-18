3655ba954b3d6cde4ac7c38fb31f8bcb76b3ec70
Patrick Lafrance <patrick.lafrance@gmail.com>
2021-11-17 20:02:54 -0500

    Réorganisé l'apparence de l'écran de login

--- app/src/components/login/login.vue
+++ app/src/components/login/login.vue
@@ -1,37 +1,33 @@
 <template>
 	<div class="container" id="login">
-		<div id="main-logo">
-			<a href="/">
-				<h1>Progression</h1>
-			</a>
-		</div>
 		<form name="login" class="form-horizontal" @submit.prevent="login">
 			<div class="form-group">
-				<label id="loginTxt" class="control-label col-sm-3">{{ $t('login.courriel') }}</label>
 				<div class="col-sm-3">
-					<input class="form-control" type="text" name="username" autofocus v-model="username" />
+					<label for="username" class="control-label">{{ $t('login.courriel') }}</label>
+					<input class="form-control" type="text" id="username" name="username" autofocus v-model="username" />
 					<div v-if="username_vide">Champ obligatoire</div>
 				</div>
 				<div class="col-sm-3">
-					<label style="text-align: left; color: #888">{{ domaine_mail }}</label>
 				</div>
 			</div>
 			<div class="form-group" v-if="password_req">
-				<label id="" class="control-label col-sm-3">{{ $t('login.motDePasse') }}</label>
 				<div class="col-sm-3">
-					<input class="form-control" name="passwd" type="password" v-model="password" />
+					<label for="passwd" class="control-label">{{ $t('login.motDePasse') }}</label>
+					<input class="form-control" id="passwd" name="passwd" type="password" v-model="password" />
 					<div v-if="password_vide">Champ obligatoire</div>
 				</div>
 			</div>
 			<br>
-			<label id="" class="control-label col-sm-3">{{ $t('login.persister') }}</label>
-			<div class="col-sm-1">
-				<input name="persister" type="checkbox" v-model="persister" />
+			<div class="form-group">
+				<div class="col-sm-3">
+					<input id="persister" name="persister" type="checkbox" v-model="persister" />
+					<label for="persister" class="control-label"  style="padding-left: 5px;">{{ $t('login.persister') }}</label>
+				</div>
 			</div>
 			<div>
 				
 			</div>
-			<div class="col-sm-offset-3 uk-margin">
+			<div class="col-sm-offset-3">
 				<input name="submit" type="submit" class="btn btn-primary" :value="$t('login.boutonConnexion') "/>
 			</div>
 
