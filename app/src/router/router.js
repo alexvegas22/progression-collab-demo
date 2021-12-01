068b9a4b60ed9608a870f0336780fcaaaa143ffa
Patrick Lafrance <plafrance@crosemont.qc.ca>
2021-11-30 19:51:14 -0500

    Ajouté la redirection vers Question lorsque URI est fournit

--- app/src/router/router.js
+++ app/src/router/router.js
@@ -31,18 +31,33 @@ const router = createRouter({
 });
 
 router.beforeEach( (to, from, next ) => {
+
+	//Redirige vers Question si le paramètre uri a été fourni
+	if(to.name == 'Home'){
+		var urlParams = new URLSearchParams(to.query);
+
+		if(urlParams.has('uri')){
+			next( {name: 'Question',
+				   query: to.query,
+				   params: { origine: to.fullPath } 
+			});
+		}
+	}
+
+	//Pour toutes les autres routes, continue
 	if(to.name != 'Question') {
 		next();
 		return;
 	}
 
-
+	//Si l'utilisateur est déjà changé, continue
 	if(store.state.user){
 		next();
 		return;
 	}
-	
+
 	const username = store.state.username || sessionStorage.getItem("username") || localStorage.getItem("username");
+	//Si un username n'est pas disponible, redirige vers la page de Login
 	if(!username){
 		next( {name: 'LoginView',
 			   query: to.query,
@@ -50,10 +65,12 @@ router.beforeEach( (to, from, next ) => {
 		});
 		return;
 	}
-	
+
+	//Charge l'utilisateur et contitnue
 	store.dispatch("getUser", process.env.VUE_APP_API_URL + "/user/" + username)
 		 .then( () => next() )
 		 .catch( () =>{
+			 //En cas de problème, redirige vers la page de Login
 			 next( {name: 'LoginView',
 					query: to.query,
 					params: { origine: to.fullPath } 
