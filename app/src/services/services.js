8ec468108fd0a16756d9be03ef19a6b09dda5d28
Patrick Lafrance <plafrance@crosemont.qc.ca>
2022-02-20 13:40:18 -0500

    Correction de bogue

--- app/src/services/services.js
+++ app/src/services/services.js
@@ -20,7 +20,7 @@ const getUserApi = async (urlUser, token) => {
 		user.liens = data.data.links;
 		user.liens.avancements = data.data.relationships.avancements.links.related;
 		user.liens.clés = data.data.relationships.cles.links.related;
-		user.avancements = [];
+		user.avancements = {};
 		if (data.included) {
 			data.included.forEach((item) => {
 				var avancement = item.attributes;
@@ -37,7 +37,7 @@ const getQuestionApi = async (urlQuestion, token) => {
 	var question = data.data.attributes;
 	question.liens = data.data.links;
 	question.tests = [];
-	question.ebauches = [];
+	question.ebauches = {};
 	if (data.included) {
 		data.included.forEach((item) => {
 			if (item.type == "test") {
@@ -136,7 +136,7 @@ function construireAvancement(data) {
 	avancement.liens.sauvegardes = data.data.relationships.sauvegardes.links.related;
 	avancement.liens.tentatives = data.data.relationships.tentatives.links.related;
 	avancement.tentatives = [];
-	avancement.sauvegardes = [];
+	avancement.sauvegardes = {};
 	if (data.included) {
 		data.included.forEach((item) => {
 			if (item.type == "tentative") {
