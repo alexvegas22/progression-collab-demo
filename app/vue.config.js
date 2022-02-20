8b5fd28fe9e160cce83cfb9b31acda50cb0d32f7
Patrick Lafrance <plafrance@crosemont.qc.ca>
2022-02-20 18:09:44 -0500

    Correction d'erreur de module

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,13 +1,15 @@
 module.exports = {
-	lintOnSave: false,
+    lintOnSave: false,
     devServer: {
-		compress: true,
-        disableHostCheck: true
-	},
+        compress: true,
+        disableHostCheck: true,
+    },
     transpileDependencies: [
         'vue-meta',
     ],
-
-	//Enlever en dev
-	publicPath: "/" + process.env.VUE_APP_SUBDIR,
+    publicPath: "/" + process.env.VUE_APP_SUBDIR,
+	node: {
+		fs: "empty",
+		path: "empty",
+	};	
 }
