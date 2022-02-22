7bd1a6fa0ff6a2ff336e24949c7ef8391402cf0a
Djane Gusty <gustyna12@live.ca>
2022-02-22 18:20:38 -0500

    ajout toggle ajouter test

--- app/src/components/question/jeu_tests/jeu_tests.vue
+++ app/src/components/question/jeu_tests/jeu_tests.vue
@@ -2,14 +2,21 @@
 	<div class="container-fluid">
 		<div class="row">
 			<div class="container col-4">
-				<h3 style="text-align: left">{{ $t("jeu_tests.jeuTests") }}</h3>
+				<h3 style="text-align: left">{{ $t("jeu_tests.jeuTests") }}  <button v-on:click="montrerAjouterTest()">+</button> </h3>
+				<!-- Quand on clique sur le bouton le input ajout un test appartait -->
+				
+				<div class="row w-100 m-0 p-1" id="champAjouterTest" style="display: none;"> 
+					<input class="col-11 test non_sélectionné p-0 m-0" placeholder="Ajouter un test" id="nomTestAjoute"> 
+					<button class="col-1">+</button>
+				</div>
+				
 				<div v-for="(test, index) in tests" :key="index">
+					
 					<Test v-bind:test="test"
 						  v-bind:index="index"
 						  v-bind:réussi="resultats[index]"
 						  v-bind:non_réussi="resultats[index] == false"
 						  v-bind:sélectionné="index==index_select"
-
 						  v-on:select="select(index)"
 						  présentation_étape="3.0"
 						/>
