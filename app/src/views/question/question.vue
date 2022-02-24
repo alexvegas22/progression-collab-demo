b2a89053ef8d432ea93def51c192f466bc1eb26b
mel <0715885@crosemont.qc.ca>
2022-02-24 10:19:33 -0500

    changement nom classe css

--- app/src/views/question/question.vue
+++ app/src/views/question/question.vue
@@ -2,13 +2,12 @@
 	<div v-if="user">
 		<Présentation v-if="démo" présentation_étape="00" />
 	</div>
-	<div>
-		<input class="form-check-input" type="checkbox" />
-	</div>
-
-	<div class="container-fluid p-3">
+	<div class="container-fluid p-3"
+        :class="{thème_Sombre_bg: thèmeSombre}">
+		<input class="form-check-input" type="checkbox" v-model="thèmeSombre"/>
 		<div class="row" v-if="avancement">
-			<div class="col-lg-4 col-md-6">
+			<div class="col-lg-4 col-md-6"
+				:class="{thème_Sombre_section: thèmeSombre}">
 				<Enonce présentation_étape="0" />
 			</div>
 			
@@ -18,7 +17,7 @@
 					<EditeurCode présentation_étape="1" style="height: 500px" />
 					<RetroactionTentative présentation_étape="3" />
 				</div>
-				<JeuTests présentation_étape="4" class="p-2" />
+				<JeuTests présentation_étape="4" class="p-2" :class="{thème_Sombre_section: thèmeSombre}" />
 			</div>
 		</div>
 	</div>
