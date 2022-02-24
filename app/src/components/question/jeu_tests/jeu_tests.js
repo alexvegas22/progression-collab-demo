1530fdc4522801e02a377f46afc1bcae2d62f288
Chatterjee Radhika <1905863@crosemont.qc.ca>
2022-02-24 11:14:01 -0500

    Internalisation a échoué, tentative pour faire marcher le pipeline

--- app/src/components/question/jeu_tests/jeu_tests.js
+++ app/src/components/question/jeu_tests/jeu_tests.js
@@ -62,15 +62,15 @@ export default {
 			if(nomTest=="") {
 				//À TESTER 
 				//alert("Nom vide")
-				alert($locales.eng.MessageErreursJeuxTest.erreurNomVide);
+				//alert($locales.eng.MessageErreursJeuxTest.erreurNomVide);
 				
 				return;	
 			};
 			//Si le nom du test est null 
 			if(nomTest==null) {
 				//À TESTER 
-				alert("Nom vide")
-				alert($locales.eng.MessageErreursJeuxTest.erreurNomExiste);
+				//alert("Nom vide")
+				//alert($locales.eng.MessageErreursJeuxTest.erreurNomExiste);
 				
 				return;	
 			};
@@ -79,7 +79,7 @@ export default {
 			if(testExiste) {
 				//À TESTER
 				//alert("Nom Existe Deja")
-				alert($locales.eng.MessageErreursJeuxTest.erreurNomExiste);
+			//	alert($locales.eng.MessageErreursJeuxTest.erreurNomExiste);
 				return;	
 			};
 			//Création d'un test 
