2eb06fb44c2437b954e3e4f1adb051493c0f5f60
Maude GervaisCloutier <mgervaiscloutier@dept-info.crosemont.quebec>
2022-03-02 17:39:55 +0000

    Delete statistiqueTest.js

--- app/src/views/statistiqueTest/statistiqueTest.js
+++
@@ -1,9 +0,0 @@
-import statistiqueTypesQuestions from "@/components/statistiqueTypesQuestions/statistiqueTypesQuestions.vue";
-
-export default {
-	name: "statistiqueTest",
-	components: {
-		statistiqueTypesQuestions,
-	},
-	
-};
