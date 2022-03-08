f0b6a11bdc1c5a0ba3ace89b631032ccdea31385
Jade Traverse-L'Heureux <1619944@crosemont.qc.ca>
2022-03-08 16:15:56 -0500

    Modification aux tailles des polices selon nos breakpoints définis. et conversion des données en px en rem.

--- app/src/components/question/enonce/enonce.vue
+++ app/src/components/question/enonce/enonce.vue
@@ -2,7 +2,7 @@
 	<div class="jumbotron encadré">
 		<div class="container">
 			<div v-if = "état_réussi" class="crochet icon icon--order-success svg">
-				<svg xmlns="http://www.w3.org/2000/svg" width="82px" height="82px">
+				<svg xmlns="http://www.w3.org/2000/svg">
 					<g>
 						<circle cx="35" cy="40" r="25"  style="fill: #8EC343; stroke: #8EC343"></circle>
 						<path d="M21.417,40.778l9.93,9.909l20.444-20.393" style="stroke-dasharray:50px, 50px; stroke-dashoffset: 0px; stroke-width: 6; stroke: white; fill: none"></path>
