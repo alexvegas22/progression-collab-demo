e2e6237f958260b7ff6b57f78615683d9f06ef60
Mathieu <xkx@hotmail.ca>
2022-03-08 16:49:33 -0500

    début tests de composants

---
+++ app/src/components/accomplissements/tableauExercices/tableauExercices.vue
@@ -0,0 +1,34 @@
+
+<template>
+    <div v-if="avancements">
+        <h1> Liste d'exercices </h1>
+        <br>
+        <div class="container">
+            <v-table class="table">
+                <thead>
+                    <tr >
+                        <th >Titre de l'exercice </th>
+                        <th >Difficulté </th>
+                        <th >Date de dernière modification <i type="button" @click="this.ordreModification(); this.$forceUpdate();" class="arrow"></i></th>
+                        <th >État </th>
+                        <th >Date de réussite <i type="button" @click="this.ordreReussite(); this.$forceUpdate();" class="arrow"></i></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="avancement in listeAvancements" :key="avancement.id">
+
+                        <td @click="this.allerQuestion(avancement.id)">{{avancement.titre}}</td>
+                        <td>{{avancement.niveau}}</td>
+                        <td>{{timestampVersDate(avancement.date_modification)}}</td>
+                        <td>{{ etat(avancement.état)}}</td>
+                        <td>{{timestampVersDate(avancement.date_réussite)}}</td> 
+                        
+                    </tr>
+                </tbody>
+            </v-table>
+        </div>
+    </div>
+</template>
+
+<style src="./tableauExercices.css"></style>
+<script src="./tableauExercices.js"></script>
---
+++ app/tests/composant/accomplissements.spec.js
@@ -0,0 +1,15 @@
+// Importation de la méthode `mount()` de Vue Test Utils
+import { mount } from '@vue/test-utils'
+import Accomplissement from "@/components/accomplissements/tableauExercices.vue";
+
+// Le composant à tester
+
+test('displays message', () => {
+  // mount() renvoie un composant Vue enveloppé avec lequel nous pouvons interagir
+  const wrapper = mount(Accomplissement);
+
+  const titre = wrapper.find('h1');
+
+  // Faire valoir le texte rendu du composant
+  expect(titre.text()).toContain('Hello world')
+})
