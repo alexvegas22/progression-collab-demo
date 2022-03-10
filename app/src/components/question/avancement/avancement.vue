8a649b052670c116203050fe4abc64a189cee796
Lachapelle Nicholas <1879282@crosemont.qc.ca>
2022-03-10 18:11:45 -0500

    ajout du dropdown des langages supportés

--- app/src/components/question/avancement/avancement.vue
+++ app/src/components/question/avancement/avancement.vue
@@ -15,8 +15,47 @@
 					</li>
 				</ul>
 			</li>
+		</ul>		
+		<button class="btn btn-secondary dropdown-toggle" type="button" id="menu_langages" data-bs-toggle="dropdown" aria-expanded="false">
+			{{ $t('avancement.ébauches') }}<!--Par défaut-->
+			<!--{{this.langage}}--><!--Dès qu'une ébauche est sélectionnée-->
+		</button>
+		<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+			<h6 class="dropdown-header">{{ $t('avancement.langages_supportés') }}</h6>
+			<li>
+				<a class="dropdown-item" href="#">Base de données</a>
+				<ul class="dropdown-menu dropdown-submenu">
+					<a class="dropdown-item">MySQL</a>		
+					<a class="dropdown-item">SSHD</a>
+				</ul>
+			</li>
+			<li>
+				<a class="dropdown-item" href="#">Scriptage</a>
+				<ul class="dropdown-menu dropdown-submenu">
+					<a class="dropdown-item">Bash</a>			
+					<a class="dropdown-item">Powershell</a>
+				</ul>
+			</li>
+			<li>
+				<a class="dropdown-item dropright" href="#">Programmation</a>
+				<ul class="dropdown-menu dropdown-submenu">
+					<li><a class="dropdown-item">C</a></li>
+					<li><a class="dropdown-item">C++</a></li>
+					<li><a class="dropdown-item">Clojure</a></li>
+					<li><a class="dropdown-item">Go</a></li>
+					<li><a class="dropdown-item">Java</a></li>
+					<li><a class="dropdown-item">JavaScript</a></li>
+					<li><a class="dropdown-item">Perl</a></li>
+					<li><a class="dropdown-item">PHP</a></li>
+					<li><a class="dropdown-item">Python</a></li>
+					<li><a class="dropdown-item">Ruby</a></li>
+					<li><a class="dropdown-item">Scala</a></li>
+					<li><a class="dropdown-item">TypeScript</a></li>
+				</ul>
+			</li>
 		</ul>
 	</div>
 </template>
 
 <script src="./avancement.js"></script>
+<style src="./avancement.css"></style>
