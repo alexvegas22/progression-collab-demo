d12d3607b49416bbb528069be55c8df508644180
Lachapelle Nicholas <1879282@crosemont.qc.ca>
2022-03-11 12:27:19 -0500

    ajout du input  dans le dropdown menu

--- app/src/components/question/avancement/avancement.vue
+++ app/src/components/question/avancement/avancement.vue
@@ -51,6 +51,16 @@
 					<li><a class="dropdown-item">Ruby</a></li>
 					<li><a class="dropdown-item">Scala</a></li>
 					<li><a class="dropdown-item">TypeScript</a></li>
+					<li><a class="dropdown-item">
+						<div class="input-group">
+							<input type="text" class="form-control" placeholder="Langage" aria-label="Input group example" aria-describedby="basic-addon1">
+							<button class="input-group-text" id="basic-addon1">
+								<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+									<path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"></path>
+								</svg>
+							</button>
+						</div>
+					</a></li>
 				</ul>
 			</li>
 		</ul>
