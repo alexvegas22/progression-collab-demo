ea9d2038b0e0136d88c33b73dac71cb34fc31dc8
bvaldiviezohuamani <bvaldiviezohuamani@dept-info.crosemont.quebec>
2022-03-15 18:34:45 -0400

    Ajustement indentation pour fenetre_info

--- app/src/components/layouts/fenetre_info.vue
+++ app/src/components/layouts/fenetre_info.vue
@@ -1,12 +1,16 @@
 <template>
-  <div style="flex: 1 1 0; min-height: 0; flex-flow: column;" :class="{thème_sombre: thèmeSombre}" class="bordure-fenetre d-flex">
-    <header class="bordure-titre p-1" :class="{thème_sombre: thèmeSombre}" v-if="titre">
-      {{ titre }}
-    </header>
-    <perfect-scrollbar>
-      <slot></slot>
-    </perfect-scrollbar>
-  </div>
+	<div
+		style="flex: 1 1 0; min-height: 0; flex-flow: column"
+		:class="{ thème_sombre: thèmeSombre }"
+		class="bordure-fenetre d-flex"
+	>
+		<header class="bordure-titre p-1" :class="{ thème_sombre: thèmeSombre }" v-if="titre">
+			{{ titre }}
+		</header>
+		<perfect-scrollbar>
+			<slot></slot>
+		</perfect-scrollbar>
+	</div>
 </template>
 
 <script src="./fenetre_info.js"></script>
