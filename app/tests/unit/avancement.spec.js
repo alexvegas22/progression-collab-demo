2a8a33e7dc3f5988ab6c0476240438bf2e29164b
Gervais-Cloutier Maude <mgervaiscloutier@dept-info.crosemont.quebec>
2022-03-16 15:04:30 -0400

    test timestampVersDate fait

--- app/tests/unit/avancement.spec.js
+++ app/tests/unit/avancement.spec.js
@@ -3,8 +3,7 @@ import Avancement from '@/components/question/avancement/avancement.js';
 
 describe('avancement.js: ', () => {
     const wrapper = mount(Avancement);
-    console.log(wrapper)
-    console.log(Avancement.methods.timestampVersDate(1647456327))
+   
     it('Test marche assurement', () => {
         expect(true).toBe(true);
     })
@@ -12,7 +11,6 @@ describe('avancement.js: ', () => {
     it('Avancemnt exist', () => {
         expect(wrapper.exists).toBeTruthy();
     })
-    
     it('étatVerChaîne selon la position', () => {
         //const spy = jest.spyOn(Avancement.methods, 'étatVersChaîne');
         expect(Avancement.methods.étatVersChaîne(0)).toBe('premièreTentative');
@@ -23,10 +21,4 @@ describe('avancement.js: ', () => {
     it('timestampVersDate', () => {
         expect(Avancement.methods.timestampVersDate(1647456327)).toBe('2022-03-16, 18 h 45 min 27 s')
     })
-    /*it('filtrerTentativesParLangage', () => {
-        const mock = jest.fn().mockReturnValue('python')
-        mock();
-        //expect(Avancement.methods.filtrerTentativesParLangage('python')).toBe('python');
-        expect(mock.data).toBe('d')
-    })*/
 })
