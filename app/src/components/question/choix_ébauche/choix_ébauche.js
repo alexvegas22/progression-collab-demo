e44c34b5e796b933c34cdab2f3bd10d156b8640c
Lachapelle Nicholas <1879282@crosemont.qc.ca>
2022-03-17 11:58:09 -0400

    avancement distinction langages avec ébauche dans la question

--- app/src/components/question/choix_ébauche/choix_ébauche.js
+++ app/src/components/question/choix_ébauche/choix_ébauche.js
@@ -1,5 +1,10 @@
 export default {
 	name: "ChoixÉbauche",
+	date() {
+		return {
+			avecÉbauche: false,
+		};
+	},
     computed: {
 		/*langage() {
 			return this.$store.state.tentative ? this.$store.state.tentative.langage : null;
@@ -10,6 +15,7 @@ export default {
 				this.sélectionnerÉbauche(langageDéfaut);
 				return this.$store.state.langageSélectionné;
 			} else {
+				console.log("Sélectionné: "+this.$store.state.langageSélectionné)
 				return this.$store.state.langageSélectionné;
 			}
 		},
@@ -18,24 +24,27 @@ export default {
 			this.créerListeLangages();
 			return this.$store.state.langagesSupportés;
 		},
-
-        tentatives() {
-			return this.$store.state.avancement.tentatives ?? [];
-		},
     },
     methods: {		
-		reinitialiserCodeEditeur(langage) {
-			this.$store.dispatch("réinitialiserÉbauche", langage);
-		},		
-		sélectionnerÉbauche(langage) {
-			this.$store.dispatch("mettreAJourLangageSelectionneÉbauche", langage);			
-		},
 		changerÉbauche(langage) {
+			//document.getElementById(langage).style.background='#6cd162';
+			this.aUneÉbauche(langage);
 			this.sélectionnerÉbauche(langage);
 			this.reinitialiserCodeEditeur(langage);
 		},
+		sélectionnerÉbauche(langage) {
+			this.$store.dispatch("mettreAJourLangageSelectionneÉbauche", langage);			
+		},
+		reinitialiserCodeEditeur(langage) {
+			if (langage == "C++") {
+				this.$store.dispatch("réinitialiserÉbauche", "cpp");
+			} else {
+				this.$store.dispatch("réinitialiserÉbauche", langage.toLowerCase());
+			}
+		},	
 		langageParDéfaut() {
-			return Object.keys(this.$store.state.question.ebauches)[0];
+			var lang = Object.keys(this.$store.state.question.ebauches)[0];
+			return lang.charAt(0).toUpperCase()+lang.slice(1);
 		},
 		nouveauLangage() {
 			var lang = document.getElementById("nouveauLangage").value;
@@ -55,65 +64,63 @@ export default {
 			return langage === null || langage.match(/^ *$/) !== null;
 		},
 		extraireLangagesAffichésSupportés(langageSupporté) {
-			console.log("Affiché : -"+langageSupporté.langageAffiché);
-			console.log("YML : -"+langageSupporté.langageYML);
-			return langageSupporté.langageAffiché;
-		},
-		getLangageAffiché(langageSupporté) {
 			return langageSupporté.langageAffiché;
 		},
 		extraireLangagesYMLSupportés(langageSupporté) {
-			console.log("Affiché : -"+langageSupporté.langageAffiché);
-			console.log("YML : -"+langageSupporté.langageYML);
 			return langageSupporté.langageYML;
-		},
-		getLangageYML(langage) {
-			var langagesSupportés = this.$store.state.langagesSupportés;
-			var langagesYML = langagesSupportés.map(this.extraireLangagesYMLSupportés);
-			
-			for(let i = 0; i < langagesYML.length; i++) {
-				if (langagesYML[i] === langage) {
-					return langagesYML[i];
-				}
-			}
-
-			console.log("YML2 : -"+langagesSupportés.langageYML);
-			//return langageSupporté.langageYML;
 		},
 		créerListeLangages(){
 			var langagesSupportés = this.$store.state.langagesSupportés;
 			var langagesÉbauchesQuestion = Object.keys(this.$store.state.question.ebauches);
 			var langagesAffichés = langagesSupportés.map(this.extraireLangagesAffichésSupportés);
-			//alert(langagesÉbauchesQuestion.length);
+			var langagesYML = langagesSupportés.map(this.extraireLangagesYMLSupportés);
+			//alert(langagesÉbauchesQuestion);
 
-			//function 
-			this.$store.dispatch("ajouterLangagesSupportés", langagesAffichés);
-//#region 
-				//for(let i = 0; i < langagesSupportés.length; i++) {
-					/*for(let j = 0; j < langagesÉbauchesQuestion.length; j++) {
-						console.log("Existing : -"+langagesÉbauchesQuestion[j]);
 
-						// Trouver comment mettre l'ébauche java dans Java de la liste
-						if (langagesÉbauchesQuestion[j] === langageSupporté.langageYML) {
-							console.log("-------------------------------------------------");
-							console.log(langageSupporté.langageAffiché);
-							console.log("-------------------------------------------------");
-							return langageSupporté.langageAffiché;
-						} else {
-							return langageSupporté.langageAffiché;
-						}
-					}*/
-					//console.log("Hardcoded : -"+langagesSupportés[i].langageYML);
-					/*if (langageSupporté.langageYML === langagesSupportés[i].langageYML){
-						//langagesAffichés.push(langagesSupportés[i].langagesAffichés);
-						return langageSupporté.langageAffiché;
-					}*/
-				//}
-//#endregion
+			for (let i = 0; i < langagesYML.length; i++) {
+				for (let o = 0; o < langagesÉbauchesQuestion.length; o++) {
+					if (langagesYML[i] === langagesÉbauchesQuestion[o]) {
+						//document.getElementById('btnLangage').style.background='#6cd162';
+						/*var btn = document.getElementById("btnLangage");
+						var e = document.createElement("i");
+						e.setAttribute("class",'<i class="bi bi-body-text"></i>')
+						e.innerHTML = '<i class="bi bi-body-text"></i>';
+						btn.appendChild(e);*/
+						//langagesAffichés[i] = " * " + langagesAffichés[i];
+					}
+				}
+			}
 
-			for(var langage of langagesÉbauchesQuestion){
+			/*for(var langage of langagesÉbauchesQuestion){
 				if(!this.$store.state.langagesSupportés.includes(langage)){
 					langagesAffichés.push(langage);
+				}
+			}*/
+			this.$store.dispatch("ajouterLangagesSupportés", langagesAffichés);
+		},
+		aUneÉbauche(langage){
+			var langagesSupportés = this.$store.state.langagesSupportés;
+			var langagesÉbauchesQuestion = Object.keys(this.$store.state.question.ebauches);
+			var langagesAffichés = langagesSupportés.map(this.extraireLangagesAffichésSupportés);
+			var langagesYML = langagesSupportés.map(this.extraireLangagesYMLSupportés);
+
+			console.log(langagesÉbauchesQuestion);
+			console.log(langagesYML);
+			console.log(langage);
+
+			for (let i = 0; i < langagesYML.length; i++) {
+				console.log("se rend ici 2");
+				
+				if (langagesAffichés[i] === langage) {
+					console.log("se rend ici 3");
+					document.getElementById(langage).style.background='#6cd162';
+					/*var btn = document.getElementById("btnLangage");
+					var e = document.createElement("i");
+					e.setAttribute("class",'<i class="bi bi-body-text"></i>')
+					e.innerHTML = '<i class="bi bi-body-text"></i>';
+					btn.appendChild(e);*/
+					//langagesAffichés[i] = " * " + langagesAffichés[i];
+					//this.avecÉbauche = true;
 				}
 			}
 		},
