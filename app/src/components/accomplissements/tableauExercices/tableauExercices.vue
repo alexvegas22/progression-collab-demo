c7ea827bd7bfaf0ff230ead31753ff3a74b01cbc
abdelghani <azebroune@example.com>
2022-03-17 14:06:30 -0400

    ajout de l'internalisation

--- app/src/components/accomplissements/tableauExercices/tableauExercices.vue
+++ app/src/components/accomplissements/tableauExercices/tableauExercices.vue
@@ -1,7 +1,7 @@
 
 <template>
     <div v-if="avancements">
-        <h1> {{ $t('accomplissements.titre') }} </h1>
+        <h1> {{ $t('accomplissements.titrePage') }} </h1>
         <br>
         <div>
             <input type="text" v-model="inputFilter" class="form-control barRecherhe">
@@ -10,10 +10,10 @@
         <div>
             <V-table :data="this.filtreAvancement" class="table">
                 <template #head>
-                    <V-th id="tglTitre" class="!triTh" sortKey="titre" @click="styleTh('tglTitre')">Titre de l'exercice</V-th>
-                    <V-th id="tglNiveau" class="!triTh" sortKey="niveau" @click="styleTh('tglNiveau')">Difficulté</V-th>
-                    <V-th id="tglDateM" class="!triTh" sortKey="date_modification" @click="styleTh('tglDateM')">Date de dernière tentative</V-th>
-                    <V-th id="tglDateR" class="!triTh" sortKey="date_réussite" @click="styleTh('tglDateR')">Date de réussite</V-th>
+                    <V-th id="tglTitre" class="!triTh" sortKey="titre" @click="styleTh('tglTitre')">{{ $t('accomplissements.titreQuestion') }}</V-th>
+                    <V-th id="tglNiveau" class="!triTh" sortKey="niveau" @click="styleTh('tglNiveau')">{{ $t('accomplissements.difficulté') }}</V-th>
+                    <V-th id="tglDateM" class="!triTh" sortKey="date_modification" @click="styleTh('tglDateM')">{{ $t('accomplissements.dateDernièreTentative') }}</V-th>
+                    <V-th id="tglDateR" class="!triTh" sortKey="date_réussite" @click="styleTh('tglDateR')">{{ $t('accomplissements.dateRéussite') }}</V-th>
                 </template>
                 <template #body="{ rows }">
                     <tr @click="allerVersQuestion(avancement.liens.self)" v-for="avancement in rows" :key="avancement.titre" class="lienQuestion">
