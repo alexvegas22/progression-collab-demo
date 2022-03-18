34e0fba4373ceca4b3c8d9af6374af7b5b7fcc07
tfock <tfock@dept-info.crosemont.quebec>
2022-03-18 00:21:35 -0400

    Refactorisation éditeur rétroaction pour tests unitaires et générale

--- app/src/components/question/rétroactions/rétroaction.js
+++ app/src/components/question/rétroactions/rétroaction.js
@@ -4,37 +4,66 @@ import parseMD from "@/util/parse";
 export default {
 	name: "Rétroaction",
 	props: {
+		test: false,
 		feedback_label: null,
 		feedback_valeur: null,
 		feedback_index: null,
-		test_index: null,	
+		test_index: 0,
 	},
 	methods: {
 		modifierFeedback(i) {
-			switch(i) {
-				case 0:
-					this.$store.state.question.tests[this.test_index].feedback.positive = this.feedback[0];
-				case 1:
-					this.$store.state.question.tests[this.test_index].feedback.négative = this.feedback[1];
-				case 2:
-					this.$store.state.question.tests[this.test_index].feedback.erreur = this.feedback[2];
+			if (this.test) {
+				switch (i) {
+					case 0:
+						this.$store.state.question.tests[this.test_index].feedback.positive = this.feedback[0];
+					case 1:
+						this.$store.state.question.tests[this.test_index].feedback.négative = this.feedback[1];
+					case 2:
+						this.$store.state.question.tests[this.test_index].feedback.erreur = this.feedback[2];
+				}
+			} else {
+				switch (i) {
+					case 0:
+						this.$store.state.question.feedback.positive = this.feedback[0];
+					case 1:
+						this.$store.state.question.feedback.négative = this.feedback[1];
+					case 2:
+						this.$store.state.question.feedback.erreur = this.feedback[2];
+				}
 			}
 		}
 	},
 	watch: {
-		feedback_valeur: function() {
-			this.feedback[0] = this.$store.state.question.tests[this.test_index].feedback.positive;
-			this.feedback[1] = this.$store.state.question.tests[this.test_index].feedback.négative;
-			this.feedback[2] = this.$store.state.question.tests[this.test_index].feedback.erreur;
+		feedback_valeur: function () {
+			if (this.test) {
+				this.feedback[0] = this.$store.state.question.tests[this.test_index].feedback.positive;
+				this.feedback[1] = this.$store.state.question.tests[this.test_index].feedback.négative;
+				this.feedback[2] = this.$store.state.question.tests[this.test_index].feedback.erreur;
+			} else {
+				this.feedback[0] = this.$store.state.question.feedback.positive;
+				this.feedback[1] = this.$store.state.question.feedback.négative;
+				this.feedback[2] = this.$store.state.question.feedback.erreur;
+			}
 		}
 	},
 	data() {
-		return {
-			feedback: [
-				this.$store.state.question.tests[this.test_index].feedback.positive,
-				this.$store.state.question.tests[this.test_index].feedback.négative,
-				this.$store.state.question.tests[this.test_index].feedback.erreur,
-			],
+		if (this.test) {
+			return {
+				feedback: [
+					this.$store.state.question.tests[this.test_index].feedback.positive,
+					this.$store.state.question.tests[this.test_index].feedback.négative,
+					this.$store.state.question.tests[this.test_index].feedback.erreur,
+				]
+			}
+		}
+		else {
+			return {
+				feedback: [
+					this.$store.state.question.feedback.positive,
+					this.$store.state.question.feedback.négative,
+					this.$store.state.question.feedback.erreur,
+				]
+			}
 		};
 	},
 };
