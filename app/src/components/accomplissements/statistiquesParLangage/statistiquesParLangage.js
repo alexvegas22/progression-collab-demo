1791fff7e65edbce65f3e4f72b4af011fd798b5c
Gervais-Cloutier Maude <mgervaiscloutier@dept-info.crosemont.quebec>
2022-03-18 13:51:29 -0400

    Méthode pour avoir les stat dynamiquement fait

--- app/src/components/accomplissements/statistiquesParLangage/statistiquesParLangage.js
+++ app/src/components/accomplissements/statistiquesParLangage/statistiquesParLangage.js
@@ -1,8 +1,6 @@
 export default {
 	name: "statistiquesParLangage",
-
 	computed: {
-
 		user() {
 			return this.$store.state.user;
 		},
@@ -10,7 +8,6 @@ export default {
 			return this.$store.state.tentativesRéussies;
 		},
 	},
-
 	mounted() {
 		return this.$store
 			.dispatch("getTentativesRéussies", {
@@ -18,14 +15,23 @@ export default {
 			})
 	},
 	methods: {
-		récupérerPourcentageRéussi : function (tentativesRéussies, langage){
+		récupérerPourcentageRéussi : function (langage){
 			let totalRéussi = 0.0;
 			let pourcentage = 0.0;
-			for(let tentative in tentativesRéussies){
-				totalRéussi += tentativesRéussies[tentative];
+			for(let tentative in this.tentativesRéussies){
+				totalRéussi += this.tentativesRéussies[tentative];
 			}
-			pourcentage = tentativesRéussies[langage] / totalRéussi * 100;
+			pourcentage = this.tentativesRéussies[langage] / totalRéussi * 100;
 			return pourcentage.toFixed(1);
+		},
+		récupérerRéussi: function(){
+			let keys = Object.keys(this.tentativesRéussies) 
+			let listeRéussi = []
+			for(let i = 0; i < keys.length; i++){
+				let pourcentage = this.récupérerPourcentageRéussi(keys[i]);
+				listeRéussi.push([keys[i], pourcentage])
+			}
+			return listeRéussi		
 		}
 	}
 };
--- app/src/components/accomplissements/statistiquesParLangage/statistiquesParLangage.vue
+++ app/src/components/accomplissements/statistiquesParLangage/statistiquesParLangage.vue
@@ -1,13 +1,8 @@
 <template>
-
-    <div v-for="(nombreRéussi, langage) in this.tentativesRéussies">
-        <pie-chart width="500px" legend="right" suffix="%" :data="[[langage, this.récupérerPourcentageRéussi(this.tentativesRéussies, langage)]]"></pie-chart>
-    </div>
-    
     <br><br><h2>{{$t('accomplissements.titreGraphParLangage')}}</h2><br>
-        <div>
-            <pie-chart width="500px" legend="right" suffix="%" :data="[['java',this.récupérerPourcentageRéussi(this.tentativesRéussies, 'java')], ['python',this.récupérerPourcentageRéussi(this.tentativesRéussies, 'python')]]"></pie-chart>
-        </div>
+     <div>
+        <pie-chart width="500px" legend="right" suffix="%" :data="this.récupérerRéussi()"></pie-chart>
+    </div>
 </template>
 
 <script src="./statistiquesParLangage.js"></script>
