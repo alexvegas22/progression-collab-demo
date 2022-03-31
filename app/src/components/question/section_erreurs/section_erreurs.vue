eecad2788afb63663d696b9cc8a0e835f49aa090
Jade Traverse-L'Heureux <1619944@crosemont.qc.ca>
2022-03-31 18:13:58 -0400

    Fix d'un oubli.

--- app/src/components/question/section_erreurs/section_erreurs.vue
+++ app/src/components/question/section_erreurs/section_erreurs.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="section-erreur">
-		<div class="p-1  height">
+		<div class="p-1">
 			<h5 class="text-danger font-weight-bold text-left card-title">
 				{{ $t("resultat_test.sortieErreur") }}
 			</h5>
