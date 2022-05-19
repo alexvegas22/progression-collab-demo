28eb35406e03da1f72d5bbb8d1cec2ad0d6e83bf
Yuhan <yuhanrt.com>
2022-05-19 13:44:58 -0400

    refactoriser le bouton de soumission dans son propre composant

---
+++ app/src/components/question/bouton_soumission/boutonSoumission.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<svg 
+			id="btn_soumettre_tentative"
+			v-shortkey="raccourcis.soumettreTentative"
+			type="button"
+			class="btn btn-valider"
+			:disabled="envoiEnCours"
+			présentation_étape="1.2"
+			@click="validerTentative"
+			@shortkey="validerTentative"
+		> 
+			<use xlink:href="./svg_bouton_play/play-button-svgrepo-com.svg#Capa_2" />
+		</svg>
+		<div :class="{spin: envoiEnCours}" />
+	</div>
+</template>
+
+<script src="./boutonSoumission.js"></script>
+<style src="./boutonSoumission.css"></style>
