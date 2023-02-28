f8db22b6dd19d6013a403237f06821871b5725b7
LTI <lti@ordralphabetix.dti.crosemont.quebec>
2023-02-27 22:50:51 -0500

    Reformatage et avance de version

--- app/src/store/getters.js
+++ app/src/store/getters.js
@@ -6,8 +6,8 @@ export default {
 	erreurs: state => state.erreurs,
 	token: state => {
 		const temps_courant = Math.round(Date.now() / 1000);
-		console.log("Temps courant: " + temps_courant)
-		console.log("Token timestamp: " + state.token.timestamp)
+		console.log("Temps courant: " + temps_courant);
+		console.log("Token timestamp: " + state.token.timestamp);
 		// Retourne le token jusqu'à 10s avant son expiration
 		return state.token && temps_courant < state.token.timestamp - 60 ? state.token.token : null;
 	}
