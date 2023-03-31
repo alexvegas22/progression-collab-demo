a8b191a37b2b9284a17c8acfe922e922c423c9f6
snely <1826841@crosemont.qc.ca>
2023-03-31 14:10:37 -0400

    Ajout des messages d'erreurs du Backend dans Login.js + I18n inclus

--- app/src/views/login/login.js
+++ app/src/views/login/login.js
@@ -40,19 +40,51 @@ export default {
 						this.$router.push({name: "Home"});
 					}
 				}
-				catch(err){
+				catch( err ){
 					if (err.response && err.response.status == 401) {
 						this.$store.dispatch("setErreurs", { message: this.$t("erreur.authentification") });
 					}
-					else if (err.response && err.response.status == 403) {
-						this.$store.dispatch("setErreurs", { message: this.$t("erreur.inscription") });
+					else if (err.response && err.response.status == 400) {
+						var msgErr = err.response.data.erreur;
+						if ("username" in msgErr && msgErr.username[0] ){
+							if (msgErr.username[0].startsWith( "Err: 1001." )){
+								this.$store.dispatch("setErreurs", { message: this.$t("erreur.inscription.usernameExistant") });
+							}
+						}
+						if ("courriel" in msgErr && msgErr.courriel[0] ){
+							if (msgErr.username[0].startsWith( "Err: 1002." )){
+								this.$store.dispatch("setErreurs", { message: this.$t("erreur.inscription.courrielExistant") });
+							}
+						}
+						if ("courriel" in msgErr && msgErr.courriel[0] ) {
+							if (msgErr.courriel[0].startsWith( "Err: 1003." )){
+								this.$store.dispatch("setErreurs", { message: this.$t("erreur.inscription.courrielInvalide") });
+							}
+						}
+						if ( ("username" in msgErr && msgErr.username[0] ) ||
+							 ("courriel" in msgErr && msgErr.courriel[0] ) ||
+							 ("password" in msgErr && msgErr.password[0] ) ||
+							 ("confirmation" in msgErr && msgErr.confirmation[0] ))
+						{
+							if (msgErr.username[0].startsWith( "Err: 1004." )){
+								this.$store.dispatch("setErreurs", { message: this.$t("erreur.inscription.champUsernameVide") });
+							}
+							if (msgErr.courriel[0].startsWith( "Err: 1004." )){
+								this.$store.dispatch("setErreurs", { message: this.$t("erreur.inscription.champCourrielVide") });
+							}
+							if (msgErr.password[0].startsWith( "Err: 1004." )){
+								this.$store.dispatch("setErreurs", { message: this.$t("erreur.inscription.champMotDePasseVide") });
+							}
+							if (msgErr.confirmation[0].startsWith( "Err: 1004." )){
+								this.$store.dispatch("setErreurs", { message: this.$t("erreur.inscription.champConfirmationVide") });
+							}
+						}
 					}
-					else{
+					else {
 						throw err;
 					}
 				}
 			})();
 		},
-
-	}
+	},
 };
