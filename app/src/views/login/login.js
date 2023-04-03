bba063f5f9ba7cfd4d1e3c4fa92b48cd7f5f3222
snely <1826841@crosemont.qc.ca>
2023-04-03 13:22:07 -0400

    Correction de views/login/login.js selon la revue de code.
    - Changer le terme msgErr pour erreurs, parce que c'est une collection d'erreurs et non un
      message d'erreur
    - Correction de la validation pour l'envoie d'information au serveur.

--- app/src/views/login/login.js
+++ app/src/views/login/login.js
@@ -45,38 +45,29 @@ export default {
 						this.$store.dispatch("setErreurs", { message: this.$t("erreur.authentification") });
 					}
 					else if (err.response && err.response.status == 400) {
-						var msgErr = err.response.data.erreur;
-						if ("username" in msgErr && msgErr.username[0] ){
-							if (msgErr.username[0].startsWith( "Err: 1001." )){
+						var erreurs = err.response.data.erreur;
+						if ("username" in erreurs && erreurs.username[0] ){
+							if (erreurs.username[0].startsWith( "Err: 1001." )){
 								this.$store.dispatch("setErreurs", { message: this.$t("erreur.inscription.usernameExistant") });
 							}
-						}
-						if ("courriel" in msgErr && msgErr.courriel[0] ){
-							if (msgErr.username[0].startsWith( "Err: 1002." )){
-								this.$store.dispatch("setErreurs", { message: this.$t("erreur.inscription.courrielExistant") });
-							}
-						}
-						if ("courriel" in msgErr && msgErr.courriel[0] ) {
-							if (msgErr.courriel[0].startsWith( "Err: 1003." )){
-								this.$store.dispatch("setErreurs", { message: this.$t("erreur.inscription.courrielInvalide") });
-							}
-						}
-						if ( ("username" in msgErr && msgErr.username[0] ) ||
-							 ("courriel" in msgErr && msgErr.courriel[0] ) ||
-							 ("password" in msgErr && msgErr.password[0] ) ||
-							 ("confirmation" in msgErr && msgErr.confirmation[0] ))
-						{
-							if (msgErr.username[0].startsWith( "Err: 1004." )){
+							else if (erreurs.username[0].startsWith( "Err: 1004." )){
 								this.$store.dispatch("setErreurs", { message: this.$t("erreur.inscription.champUsernameVide") });
 							}
-							if (msgErr.courriel[0].startsWith( "Err: 1004." )){
+						}
+						if ("courriel" in erreurs && erreurs.courriel[0] ){
+							if (erreurs.courriel[0].startsWith( "Err: 1002." )){
+								this.$store.dispatch("setErreurs", { message: this.$t("erreur.inscription.courrielExistant") });
+							}
+							else if (erreurs.courriel[0].startsWith( "Err: 1003." )){
+								this.$store.dispatch("setErreurs", { message: this.$t("erreur.inscription.courrielInvalide") });
+							}
+							else if (erreurs.courriel[0].startsWith( "Err: 1004." )){
 								this.$store.dispatch("setErreurs", { message: this.$t("erreur.inscription.champCourrielVide") });
 							}
-							if (msgErr.password[0].startsWith( "Err: 1004." )){
+						}
+						if ("password" in erreurs && erreurs.password[0] ) {
+							if (erreurs.password[0].startsWith( "Err: 1004." )){
 								this.$store.dispatch("setErreurs", { message: this.$t("erreur.inscription.champMotDePasseVide") });
-							}
-							if (msgErr.confirmation[0].startsWith( "Err: 1004." )){
-								this.$store.dispatch("setErreurs", { message: this.$t("erreur.inscription.champConfirmationVide") });
 							}
 						}
 					}
