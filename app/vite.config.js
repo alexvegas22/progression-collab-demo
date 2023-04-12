2657342b2d93440ec46777aa6d2fa8b41749a275
Patrick Lafrance <plafrance@crosemont.qc.ca>
2023-04-12 16:06:15 +0000

    Patch revert versions

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import { resolve } from 'path';
+const path = require("path");
 
 export default defineConfig({
 	plugins: [vue()],
 	resolve: {
 		extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
 		alias: {
-			"@": resolve(__dirname, "./src"),
+			"@": path.resolve(__dirname, "./src"),
 		},
 	},
 	server: {
