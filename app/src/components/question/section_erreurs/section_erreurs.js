2cbfb0cf71993a9bec401e5712c7bc0632dbe6b3
Patrick Lafrance <plafrance@crosemont.qc.ca>
2023-09-19 23:43:27 +0000

    Ajout d'une barre de défilement au panneau d'ereurs

--- app/src/components/question/section_erreurs/section_erreurs.js
+++ app/src/components/question/section_erreurs/section_erreurs.js
@@ -1,6 +1,7 @@
 import Ampoule from "@/components/question/ampoule/ampoule.vue";
+import FenêtreInfo from "@/components/layouts/fenetre_info.vue";
 export default {
-	components: {Ampoule},
+	components: {Ampoule, FenêtreInfo},
 	props: {
 		test: null,
 		résultat: null,
