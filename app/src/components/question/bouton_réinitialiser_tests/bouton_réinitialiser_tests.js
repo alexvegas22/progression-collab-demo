05d73c416c38f9e3b700846b8348b4d1b8f606ff
Patrick Lafrance <plafrance@crosemont.qc.ca>
2023-10-19 23:57:20 -0400

    Fichiers manquants

---
+++ app/src/components/question/bouton_réinitialiser_tests/bouton_réinitialiser_tests.js
@@ -0,0 +1,20 @@
+export default {
+	name: "BoutonRéinitialiserTests",
+	computed: {
+		resultats() {
+			return this.$store.state.tentative?.resultats;
+		},
+		tests() {
+			return this.$store.state.question?.tests;
+		},
+		dirty(){
+			return this.resultats?.filter( t => t!=null ).length + this.tests.filter( t => t.dirty===true ).length;
+		},
+	},
+	methods: {
+		réinitialiserTests() {
+			this.$store.dispatch("réinitialiserTests");
+			this.$store.dispatch("setRésultats", []);
+		}
+	}
+}
